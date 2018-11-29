6a0c616767979b62cdeab1b1bc50381e8e49992e
bentkotch <44276835+bentkotch@users.noreply.github.com>
2018-11-28 21:20:23 -0500

    Add files via upload

--- _CAPSTONE/index.js
+++ _CAPSTONE/index.js
@@ -2,6 +2,37 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "capstone"
+});
+
 app.use(express.static('public'))
 
 app.listen(port, () => console.log(`Capstone app listening on port ${port}!`))
+
+app.get('/api', function(req, res) {
+  res.send('hello world')
+})
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/api', function (req, res) {
+ res.send('hello world')
+})
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to Server");
+});
+
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM maps", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
