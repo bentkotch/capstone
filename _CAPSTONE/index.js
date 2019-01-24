c010ca76aff2a583e53384a9c24bb7fa6e49fdf6
Ben Kotch <bentkotch@gmail.com>
2019-01-23 23:05:48 -0500

    Began process of promisifying the node.js calls to mysql. As of now, the correct data is being returned, but as [RowDataPacket (JSON)]. Next step is to remove the object from the array and convert it to a JSON object the front end can process correctly.

--- _CAPSTONE/index.js
+++ _CAPSTONE/index.js
@@ -2,36 +2,56 @@ const express = require('express')
 const app = express()
 const port = 80
 
-var mysql = require('mysql');
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "capstone",
-  database: "capstone"
-});
+var mysql = require('promise-mysql');
+
+//var mysql = require('mysql');
+//var con = mysql.createConnection({
+//  user: "root",
+//  password: "capstone",
+//  database: "capstone"
+//});
 
 // Connect to server
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected to Server");
-});
+//con.connect(function(err) {
+//  if (err) throw err;
+//  console.log("Connected to Server");
+//});
 
 app.use(express.static('public'))
 
 // respond with json from maps table when a GET request is made to the /api/maps
-app.get('/api/maps', function (req, res) {
-  con.query("SELECT * FROM maps", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-    res.json(result);
-  });
-})
+//app.get('/api/maps', function (req, res) {
+//  con.query("SELECT * FROM maps", function (err, result, fields) {
+//    if (err) throw err;
+//    console.log(result);
+//    res.json(result);
+//  });
+//})
 
 app.get('/api/maps/:map_id', function (req, res) {
-  con.query("SELECT * FROM maps WHERE map_id = ?", [req.params.map_id], function (err, result, fields) {
-    if (err) throw err;
-    console.log(result[0]);
-    res.json(result[0]); //removes JSON object from array
+  var connection;
+  mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "capstone",
+    database: "capstone"
+  }).then(function(conn){
+      connection = conn;
+      var map = connection.query('SELECT * FROM maps WHERE map_id = ?', [req.params.map_id]);
+      return map;
+  //}).then(function(){
+    //  var result = connection.query('SELECT * FROM data_points WHERE map_id = ?', [req.params.map_id]);
+    //  connection.end();
+    //  return result;
+  }).then(function(rows){
+      console.log("function(rows) result: ");
+      console.log(rows);
+
+      res.sendJson;
+  }).catch(function(error){
+      if (connection && connection.end) connection.end();
+      //logs out the error
+      console.log(error);
   });
 })
 
